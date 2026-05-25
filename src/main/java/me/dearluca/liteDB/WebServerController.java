222dc7b4d59a8bef2a75aa0ee2526e3738a6b2af
Luca Marchi <luca1010.marchi@gmail.com>
2026-05-25 18:23:19 +0200

    feat: put and get

--- src/main/java/me/dearluca/liteDB/WebServerController.java
+++ src/main/java/me/dearluca/liteDB/WebServerController.java
@@ -1,13 +1,35 @@
 package me.dearluca.liteDB;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WebServerController {
+    private final KeyValueStore store;
+
+    public WebServerController(KeyValueStore store) {
+        this.store = store;
+    }
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello world";
     }
+
+    @PutMapping("/kv/{key}")
+    public ResponseEntity<Void> put(
+            @PathVariable String key,
+            @RequestBody String value
+    ) {
+        store.put(key, value);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/kv/{key}")
+    public String get(
+            @PathVariable String key
+    ) {
+        return store.get(key);
+    }
+
 }
