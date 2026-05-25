75c32e50d2e88ce2937420c8e93d5351c10ddad2
Luca Marchi <luca1010.marchi@gmail.com>
2026-05-25 18:04:16 +0200

    feat: concurrent key-value store

---
+++ src/main/java/me/dearluca/liteDB/KeyValueStore.java
@@ -0,0 +1,18 @@
+package me.dearluca.liteDB;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class KeyValueStore {
+    private final ConcurrentHashMap<String, String> store = new ConcurrentHashMap<>();
+
+    public void put(String key, String value) {
+        store.put(key, value);
+    }
+
+    public String get(String key) {
+        return store.get(key);
+    }
+}
