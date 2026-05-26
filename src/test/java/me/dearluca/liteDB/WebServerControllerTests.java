d0534767e32b370c367985e8b7747f02a4fb0f37
Luca Marchi <luca1010.marchi@gmail.com>
2026-05-26 11:30:01 +0200

    test: put then get

--- src/test/java/me/dearluca/liteDB/WebServerControllerTests.java
+++ src/test/java/me/dearluca/liteDB/WebServerControllerTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,5 +27,16 @@ public class WebServerControllerTests {
     void testPut() throws Exception {
         mockMvc.perform(put("/kv/user:2").content("Mario"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPutThenGet() throws Exception {
+        mockMvc.perform(put("/kv/user:3")
+                        .content("John"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/kv/user:3"))
+                .andExpect(status().isOk())
+                .andExpect(result -> content().string("John"));
     }
 }
