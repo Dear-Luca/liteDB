4755bd3416177b3d06500aaccedd40928528a434
Luca Marchi <luca1010.marchi@gmail.com>
2026-06-03 17:45:40 +0200

    chore: protobuf + grpc dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.google.protobuf.gradle.id
+
 plugins {
 	java
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.protobuf") version "0.9.5"
 }
 
 group = "me.dearluca"
@@ -17,11 +20,44 @@ repositories {
 	mavenCentral()
 }
 
+extra["springGrpcVersion"] = "1.0.3"
+
 dependencies {
+	implementation("io.grpc:grpc-services")
+	implementation("org.springframework.grpc:spring-grpc-client-spring-boot-starter")
+	implementation("org.springframework.grpc:spring-grpc-server-spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.grpc:spring-grpc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.grpc:spring-grpc-dependencies:${property("springGrpcVersion")}")
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc") {
+					option("@generated=omit")
+				}
+			}
+		}
+	}
+}
+
 
 tasks.withType<Test> {
 	useJUnitPlatform()
