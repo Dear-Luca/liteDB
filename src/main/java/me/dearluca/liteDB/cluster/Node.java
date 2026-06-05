936a680ea7d6f6a1679bcb3e05ef8d6a62434aa1
Luca Marchi <luca1010.marchi@gmail.com>
2026-06-05 12:21:10 +0200

    chore: node doc

--- src/main/java/me/dearluca/liteDB/cluster/Node.java
+++ src/main/java/me/dearluca/liteDB/cluster/Node.java
@@ -1,5 +1,11 @@
 package me.dearluca.liteDB.cluster;
 
+/**
+ * Represents a node in the cluster.
+ * @param id the unique identifier of the node.
+ * @param host the hostname or IP address of the node.
+ * @param port the port number on which the node is listening for gRPC requests.
+ */
 public record Node(
         String id,
         String host,
--- src/main/java/me/dearluca/liteDB/cluster/NodeProperties.java
+++ src/main/java/me/dearluca/liteDB/cluster/NodeProperties.java
@@ -4,6 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+/**
+ * Configuration properties for the cluster nodes.
+ * @param nodeId the unique identifier of the node.
+ * @param grpcPort the port number on which the node is listening for gRPC requests.
+ * @param nodes the list of nodes in the cluster.
+ */
 @ConfigurationProperties(prefix = "litedb")
 public record NodeProperties(
         String nodeId,
