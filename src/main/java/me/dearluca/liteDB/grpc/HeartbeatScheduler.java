8b411bdad504514aa97fd7502163ac1d3386faab
Luca Marchi <luca1010.marchi@gmail.com>
2026-06-15 16:20:43 +0200

    feat: failure detection

--- src/main/java/me/dearluca/liteDB/grpc/HeartbeatScheduler.java
+++ src/main/java/me/dearluca/liteDB/grpc/HeartbeatScheduler.java
@@ -2,7 +2,7 @@ package me.dearluca.liteDB.grpc;
 
 import me.dearluca.liteDB.cluster.Node;
 import me.dearluca.liteDB.cluster.NodeProperties;
-import me.dearluca.liteDB.controller.KVStoreController;
+import me.dearluca.liteDB.cluster.NodeStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HeartbeatScheduler {
-    private static final int HEARTBEAT_RATE = 2000;
+    private static final int HEARTBEAT_RATE = 5000;
     private final NodeProperties nodeProperties;
     private final NodeClient replicationClient;
     private static final Logger log = LoggerFactory.getLogger(HeartbeatScheduler.class);
@@ -28,8 +28,15 @@ public class HeartbeatScheduler {
                 continue;
             }
             log.info("[GRPC] HEARTBEAT to {}", node.id());
-            boolean alive = replicationClient.heartbeat(node);
-            log.info("[GRPC] HEARTBEAT result node={} alive={}", node.id(), alive);
+            boolean isAlive = replicationClient.heartbeat(node);
+            log.info("[GRPC] HEARTBEAT result node={} alive={}", node.id(), isAlive);
+            if (!isAlive && node.getNodeStatus() == NodeStatus.UP) {
+                node.setNodeStatus(NodeStatus.DOWN);
+                log.warn("Node {} changed state: UP -> DOWN", node.id());
+            } else if (isAlive && node.getNodeStatus() == NodeStatus.DOWN) {
+                node.setNodeStatus(NodeStatus.UP);
+                log.info("Node {} changed state: DOWN -> UP", node.id());
+            }
         }
     }
 }
